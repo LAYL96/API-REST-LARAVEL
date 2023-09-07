a6b85effea38b13ae3f2ac1484fb712c5ae2f38d
Luis_Yos <130430932+LAYL96@users.noreply.github.com>
2023-09-07 17:31:20 -0600

    se agrego las validaciones de cada campo y los errores de lo contrario estuvieran mal colocados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,34 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
-        
+    public function register(Request $request)
+    {
+
         // Recoger los datos del usuario por post
         $json = $request->input('json', null);
         $params = json_decode($json); // objeto
         $params_array = json_decode($json, true); // array
 
-        // Validar datos
+        if (!empty($params) && !empty($params_array)) {
+            // Limpiar datos
+            $params_array = array_map('trim', $params_array);
 
-        // Cifrar la contraseña
+            // Validar datos
+            $validate = Validator::make($params_array, [
+                'name'      => 'required|alpha',
+                'surname'   => 'required|alpha',
+                'email'     => 'required|email',
+                'password'  => 'required'
+            ]);
 
-        // Comprobar si el usuario ya existe (duplicado)
+            if ($validate->fails()) {
+                // La validacion ha fallado
+                $data = array(
+                    'status' => 'error',
+                    'code' => 404,
+                    'message' => 'El usuario no se ha creado',
+                    'errors' => $validate->errors()
+                );
+            } else {
+                // validacion pasada correctamente
 
-        // Crear el usuario
+                // Cifrar la contraseña
 
-        $data = array(
-            'status' => 'error',
-            'code' => '404',
-            'message' => 'El usuario no se ha creado'
-        );
+                // Comprobar si el usuario ya existe (duplicado)
 
+                // Crear el usuario
+
+                $data = array(
+                    'status' => 'success',
+                    'code' => 200,
+                    'message' => 'El usuario se ha creado correctamente'
+                );
+            }
+        } else {
+            $data = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Los datos enviados no son correctos'
+            );
+        }
         return response()->json($data, $data['code']);
     }
 
-    public function login(Request $request){
-
+    public function login(Request $request)
+    {
     }
 }
