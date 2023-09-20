da4d137c917e4a93ab8b50f6b3104af148adfc65
LAYL <130430932+LAYL96@users.noreply.github.com>
2023-09-19 19:29:41 -0600

    se agrego el login de usuarios, en la accion del controlador

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,10 +75,37 @@ class UserController extends Controller
     {
         $JwtAuth = new JwtAuth();
 
-        $email = 'juan@juan.com';
-        $password = 'juan';
-        $pwd = hash('sha256', $password);
+        // Recibir datos por POST
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
 
-        return $JwtAuth->signup($email, $pwd);
+        // Validar esos datos
+        $validate = Validator::make($params_array, [
+            'email'     => 'required|email', 
+            'password'  => 'required'
+        ]);
+
+        if ($validate->fails()) {
+            // La validacion ha fallado
+            $signup = array(
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'El usuario no se ha podido identificar',
+                'errors' => $validate->errors()
+            );
+        } else {
+            // Cifrar la password
+            $pwd = hash('sha256', $params->password);
+
+            // Devolver token o datos
+            $signup = $JwtAuth->signup($params->email, $pwd);
+
+            if(!empty($params->gettoken)){
+                $signup = $JwtAuth->signup($params->email, $pwd, true);
+            }
+        }
+
+        return response()->json($signup, 200); 
     }
 }
